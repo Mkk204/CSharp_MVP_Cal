bf3ea360b1f38ca86e9992195bf0a7b2bce4c86d
mkk <monkeykk204@gmail.com>
2025-02-25 14:25:54 +0800

    20250225- fix chained operators error

--- Calculater/Calculater/Calculator.cs
+++ Calculater/Calculater/Calculator.cs
@@ -44,83 +44,93 @@ namespace Calculater
             temp = Convert.ToChar(clickOper.Text);
             count++;
             //判斷運算子前一個是否為數字，成立的話 持續抓數字
-            if ((lastchar > 47 && lastchar < 58) && count!= 2 )
-            {               
+            if ((lastchar > 47 && lastchar < 58) && count!= 2)
+            {
                 display.Text += clickOper.Text;
             }
                        
-            else if (count==2)
+            else if ((lastchar > 47 && lastchar < 58) && count ==2)
             {              
                 object mySender = "="; // 這裡可以放任何你想要的 sender
                 EventArgs myEventArgs = new EventArgs(); // 如果不需要額外參數，就這樣傳                
-                Equal_Click(mySender, myEventArgs); // 手動觸發事件  
+                Equal_Click(mySender, myEventArgs); // 手動觸發Equal_Click事件  
             }
             //更改運算子
             else
             {
+                count--;
                 display.Text = display.Text.Remove(display.Text.Length - 1);
                 display.Text += clickOper.Text;             
             }
             temp2 = temp;
         }
-
+        
         //抓數字和輸出運算結果
         private void Equal_Click(object sender, EventArgs e)
         {
             string displaytemp = display.Text;
             string[] parts;
-            //分割字串數字出來
-            if (count == 2)
+
+            if (double.TryParse(display.Text, out double result)) // 如果是數字和= 的組合，則return
             {
-                parts = display.Text.Split(temp2);
-                temp3 = temp2;
+                return; // 不執行後續程式
             }
             else
-            {
-                parts = display.Text.Split(temp);
-                temp3 = temp;
-            }          
-            Sum = double.Parse(parts[0]);
-            Num = double.Parse(parts[1]);
+            {                
+                //分割字串數字出來
+                if (count == 2)
+                {
+                    parts = display.Text.Split(temp2);
+                    temp3 = temp2;
+                }
+                else
+                {
+                    parts = display.Text.Split(temp);
+                    temp3 = temp;
+                }
+                Sum = double.Parse(parts[0]);
+                Num = double.Parse(parts[1]);
 
-            switch (temp3)
-            {
-                case '+':
-                    Sum += Num;
-                    break;
-                case '-':
-                    Sum -= Num;
-                    break;
-                case '*':
-                    Sum *= Num;
-                    break;
-                case '/':
-                    Sum /= Num;                   
-                    break;
-                case '%':
-                    Sum %= Num;
-                    break;
-                default:
-                    Num = Double.Parse(display.Text);                   
-                    break;
-            }
+                switch (temp3)
+                {
+                    case '+':
+                        Sum += Num;
+                        break;
+                    case '-':
+                        Sum -= Num;
+                        break;
+                    case '*':
+                        Sum *= Num;
+                        break;
+                    case '/':
+                        Sum /= Num;
+                        break;
+                    case '%':
+                        Sum %= Num;
+                        break;
+                    default:
+                        Num = Double.Parse(display.Text);
+                        break;
+                }
 
-            if (Num == 0 && temp=='/')
-            {
-                this.display.Font = new System.Drawing.Font("新細明體", 15F);
-                display.Text = "無法除零，請按AC歸零";
+                if (Num == 0 && temp == '/')
+                {
+                    this.display.Font = new System.Drawing.Font("新細明體", 15F);
+                    display.Text = "無法除零，請按AC歸零";
+                }
+                else if (count == 2)
+                {
+                    count = 1;
+                    display.Text = Convert.ToString(Sum) + temp;
+                }
+                else
+                {
+                    count = 0;
+                    display.Text = Convert.ToString(Sum);
+                }
+                histext.Text += displaytemp + "=" + Sum + "\r\n";
             }
-            else if (count == 2)
-            {
-                count = 1;
-                display.Text = Convert.ToString(Sum) + temp;
-            }
-            else
-            {
-                count = 0;
-                display.Text = Convert.ToString(Sum);
-            }
-            histext.Text += displaytemp + "=" + Sum + "\r\n";
+            
         }
 
         //清除歸零 AC
